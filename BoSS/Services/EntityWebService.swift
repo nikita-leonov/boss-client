2eb6a217b09e7b92f6f06ce797231823b025b4db
erudel <emanuele.rudel@buddyhopp.com>
2015-02-28 16:26:34 -0800

    APP-4 | Implement submissions service - part1

---
+++ BoSS/Services/EntityWebService.swift
@@ -0,0 +1,27 @@
+//
+//  EntityWebService.swift
+//  BoSS
+//
+//  Created by Emanuele Rudel on 28/02/15.
+//  Copyright (c) 2015 Bureau of Street Services. All rights reserved.
+//
+
+import Foundation
+import AFNetworking
+import ReactiveCocoa
+import AFNetworking_RACExtensions
+
+
+class EntityWebService {
+    
+    private let sessionManager: AFURLSessionManager
+    
+    required init(sessionManager: AFURLSessionManager) {
+        self.sessionManager = sessionManager
+    }
+    
+    internal func read(path: String, parameters: AnyObject? = nil, page: [String: String]? = nil) -> RACSignal? {
+        return nil
+    }
+    
+}
